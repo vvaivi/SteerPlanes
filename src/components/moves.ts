548cce85aa98aadab2f44f6205c8be1fedf943d1
vvaivi <viskari.aino@gmail.com>
2022-11-26 19:43:21 +0200

    calculate distance and direction

--- src/components/moves.ts
+++ src/components/moves.ts
@@ -1,5 +1,15 @@
-import { Aircraft, Airport, NoPlaneState } from '../types'
+import { Aircraft, Airport, NoPlaneState, Point } from '../types'
+import { normalizeHeading } from '../utils/math'
 import { findLandingPoint } from './path'
+
+const getDirection = (pointFrom: Point, pointTo: Point) => {
+    const direction = Math.atan2(pointTo.y - pointFrom.y, pointTo.x - pointFrom.x) * (180 / Math.PI) //Convert to degrees
+    return normalizeHeading(direction)
+}
+  
+const getDistance = (pointFrom: Point, pointTo: Point) => {
+return Math.sqrt(Math.pow(pointTo.x - pointFrom.x, 2) + Math.pow(pointTo.y - pointFrom.y, 2))
+}
 
 export const findDestination = (gameState: NoPlaneState, aircraft: Aircraft) => {
   const destination = gameState.airports.find((airport: Airport) => airport.name === aircraft.destination)
@@ -12,3 +22,4 @@ export const findDestination = (gameState: NoPlaneState, aircraft: Aircraft) => 
 
   findLandingPoint(aircraft)
 }
+
